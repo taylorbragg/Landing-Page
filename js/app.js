9da8a34cd23be2c3ba7bcf6badcfaf8e019e2eb0
taylorbragg <J02255381@jwu.edu>
2021-11-15 20:55:00 -0500

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,17 @@
+/* EMAIL FORM HANDLING */
+
+$('form').submit(function(e){
+  // do not submit form
+  e.preventDefault();
+  // save value if the form field is not blank or the default
+  if( $('#email').val() !== '' && $('#email').val() !== 'Email' ){
+    // store value added to field in a variable
+    let val = $('#email').val();
+    // add email address to message
+    $('.form-confirmation-message span').text(val);
+    // show message (initially hidden with "hide" class)
+    $('.form-confirmation-message').removeClass('hide');
+    // hide input
+    $('.form-input').addClass('hide');
+  }
+})
